99c482d697272bbe225c30d27d7452e76209e0aa
MingJie69 <heemingjie387@gmail.com>
2026-03-07 16:56:57 +0800

    bugfix

--- final/asgi.py
+++ final/asgi.py
@@ -1,16 +1,23 @@
 import os
+import django # 必须导入 django
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import elearning.routing # 我们下一步会创建这个文件
 
+# 1. 设置环境变量
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'final.settings')
 
+# 2. 关键：必须在导入任何 routing 或 models 之前调用 django.setup()
+django.setup()
+
+# 3. 现在可以安全地导入业务代码了
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import elearning.routing 
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(), # 处理普通的 HTTP 请求
+    "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            elearning.routing.websocket_urlpatterns # 处理 WebSocket 请求
+            elearning.routing.websocket_urlpatterns
         )
     ),
 })
